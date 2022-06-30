b5b13b3db86581546810dc3c1c1594e23db217fe
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-06-30 13:09:33 -0500

    clase 62

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
   //* shuffle cards
   const shuffleCards = () => {
@@ -26,7 +28,10 @@ function App() {
     setTurns(0)
   }
 
-  console.log('cars:', cards , ' turns:', turns)
+  //* handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
 
   return (
     <div className="App">
@@ -35,7 +40,11 @@ function App() {
       <div className="card-grid">
         {
           cards.map(card => (
-            <SingleCard card={card} key={card.id}/>
+            <SingleCard
+              card={card}
+              key={card.id}
+              handleChoice={handleChoice}
+            />
           ))
         }
       </div>
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,13 +1,22 @@
 import './SingleCard.css'
 
 export const SingleCard = props => {
-  const { card } = props
+  const { card, handleChoice } = props
+
+  const handleClick = () => {
+    handleChoice(card)
+  }
 
   return (
     <div className="card">
       <div>
         <img src={card.src} alt="card front" className='front' />
-        <img src="/img/cover.png" alt="card back" className='back' />
+        <img
+          onClick={handleClick}
+          src="/img/cover.png"
+          alt="card back"
+          className='back'
+        />
       </div>
     </div>
   )
